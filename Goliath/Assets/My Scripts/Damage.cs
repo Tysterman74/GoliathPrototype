5d9de482c165d8d66c238e3b98905cb62aeed313
Khai Nguyen <khai.b.nguyen@gmail.com>
2014-11-18 15:18:36 -0800

    Added additional prefabs

---
+++ Goliath/Assets/My Scripts/Damage.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+
+    public int damage = 1;
+    public int multiplier = 1;
+    
+	// Use this for initialization
+	void Start () {
+  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Goliath/Assets/My Scripts/Damageable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Class that allows objects to take damage
+public class Damageable : MonoBehaviour {
+
+    public int health = 10;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0)
+        {
+            Die();
+        }
+	}
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Weakness hit");
+        if (col.gameObject.CompareTag("Damage"))
+        {
+            Damage attack = col.gameObject.GetComponent<Damage>();
+            
+            health = health - attack.damage * attack.multiplier;
+            Debug.Log(col.gameObject.name);
+        }
+    }
+
+    void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
